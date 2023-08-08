991a0d8d4e8f8a97f8b249a263ec1a29a15d6b7c
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-08-08 15:34:17 -0500

    integrando Socket.io - mediante documentacion oficial

--- CLASE10/desafio10/src/app.js
+++ CLASE10/desafio10/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import realTimeProductsRoutes from './routes/realtimeproducts.routes.js';
-import homeRoutes from './routes/home.routes.js';
+import productRoute from './routes/products.router.js';
+import carritoRoute from './routes/cart.router.js';
+import realTimeProductsRoutes from './routes/realtimeproducts.router.js';
+import homeRoutes from './routes/home.router.js';
 import { __dirname } from './utils.js';
 import { engine } from 'express-handlebars';
+import { Server } from "socket.io";
 
 const app = express();
 
@@ -17,10 +20,20 @@ app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
 
 // configuracion de rutas:
+app.use('/api/products', productRoute);
+app.use('/api/carts', carritoRoute);
 app.use('/realtimeproducts', realTimeProductsRoutes);
 app.use('/', homeRoutes);
 
 const PORT = 8080;
-app.listen(PORT, () => {
+const httpServer = app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+    console.log('Nueva conexion establecida!');
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado!');
+    });
 });
